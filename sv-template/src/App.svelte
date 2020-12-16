09ae3a896be7db24217bb2078745df1f886cbd41
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2020-12-15 18:35:19 -0700

    beginning run

---
+++ sv-template/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+import Currentsection from "./components/currentsection.svelte";
+
+
+</script>
+
+<main>
+	<Currentsection/>
+</main>
+
+<style>
+
+</style>
---
+++ sv-template/src/components/pageLayouts/PageDataPortions.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let productItem;
+    //export the item product item
+    console.log(productItem)
+</script>
+
+<style>
+
+</style>
+
+<li>
+    {productItem.title}
+</li>
